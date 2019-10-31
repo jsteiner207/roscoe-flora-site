fb9979b6a6e51c5792cd8f209043eddcf7ccb79e
Mohammed <44106897+Mokoan@users.noreply.github.com>
2019-10-31 12:10:45 -0500

    1st attempt gallery-non-Fuctional
    found a js and applying it to the website, purpose is to grab instagram feed from Rosca flora, and show case current work
    called Instafeed.js

--- src/Components/pages/Portfolio.js
+++ src/Components/pages/Portfolio.js
@@ -5,7 +5,17 @@ import React from 'react'
 export default function Portfolio() {
     return (
         <div>
-            <h2 style={linkstyle}>Portfolio is under constructoin</h2>
+            <h2 style={linkstyle}>Portfolio is under deat</h2>
+            <script type="text/javascript">
+    var feed = new Instafeed(
+        get: 'popular',
+        
+        clientId: '10420343974',
+        template: '<a class="animation" href="{{link}}"><img src="{{image}}" /></a>'
+    });
+    feed.run();
+</script>
+<script type="text/javascript" src="path/to/instafeed.min.js"></script>
         </div>
     )
 }
