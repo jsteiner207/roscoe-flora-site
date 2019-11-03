03a821ad53b6233a7e8e83460f89c4f6e02ae234
Mohammed <44106897+Mokoan@users.noreply.github.com>
2019-11-03 15:25:07 -0600

    blank Portfolio (broken links)

--- src/Components/pages/Portfolio.js
+++ src/Components/pages/Portfolio.js
@@ -1,10 +1,42 @@
 import React from 'react'
-
-
-
+import { Gallery, GalleryImage } from "react-photo-gallery";
+//import { photos } from "\Photos.js";
+import PhotoGrid from "react-photo-feed";
+import "react-photo-feed/library/style.css";
+const demoPhotos = [
+	{
+		id : 1, src : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_n.jpg",
+		bigSrc : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_b.jpg"
+	},
+	{
+		id : 2, src : "https://farm5.staticflickr.com/4240/35527849422_25a0a67df6_n.jpg",
+		bigSrc : "https://farm5.staticflickr.com/4240/35527849422_25a0a67df6_b.jpg"
+	},
+	{
+		id : 3, src : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_n.jpg",
+		bigSrc : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_b.jpg"
+	},
+	{
+		id : 4, src : "https://farm5.staticflickr.com/4240/35527849422_25a0a67df6_n.jpg",
+		bigSrc : "https://farm5.staticflickr.com/4240/35527849422_25a0a67df6_b.jpg"
+	},
+	{
+		id : 5, src : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_n.jpg",
+		bigSrc : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_b.jpg"
+	},
+	{
+		id : 6, src : "https://farm5.staticflickr.com/4240/35527849422_25a0a67df6_n.jpg",
+		bigSrc : "https://farm5.staticflickr.com/4240/35527849422_25a0a67df6_b.jpg"
+	},
+	{
+		id : 7, src : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_n.jpg",
+		bigSrc : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_b.jpg"
+	}
+]
 export default function Portfolio() {
     return (
         <div>
+<<<<<<< Updated upstream
             <h2 style={linkstyle}>Portfolio is under deat</h2>
             <script type="text/javascript">
     var feed = new Instafeed(
@@ -16,10 +48,25 @@ export default function Portfolio() {
     feed.run();
 </script>
 <script type="text/javascript" src="path/to/instafeed.min.js"></script>
+=======
+            <h2 style={linkstyle}>Portfolio is under constructoin</h2>
+            
+            <PhotoGrid columns={3} photos={demoPhotos} />
+>>>>>>> Stashed changes
         </div>
     )
 }
+//import React from "react";
 
+
+
+
+// ReactDOM.render(
+// 	<div>
+// 		<PhotoGrid columns={3} photos={demoPhotos} />
+// 	</div>,
+// 	document.getElementById('root')
+// );
 
 const linkstyle = {
     color: '#333',
