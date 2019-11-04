9ee9347f3b8ab37aa0bd1367aa66fb29c620c231
Mohammed <44106897+Mokoan@users.noreply.github.com>
2019-11-04 17:26:25 -0600

    Added Portfolio Gallery

--- src/Components/pages/Portfolio.js
+++ src/Components/pages/Portfolio.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
 
 
+	
 
-export default function Portfolio() {
-    return (
-        <div>
-            <h2 style={linkstyle}>Portfolio is under constructoin</h2>
-        </div>
-    )
+export default function Portfolio () {
+   
+       
+	const AutoplaySlider = withAutoplay(AwesomeSlider);
+		const slider = (
+		  <AutoplaySlider
+			play={true}
+			cancelOnInteraction={false} // should stop playing on user interaction
+			interval={6000}
+			infinite = {true}
+		  >
+	<div data-src="https://i.imgur.com/IHu4dkl.png" />
+    <div data-src="https://i.imgur.com/qkCCZN2.png" />
+    <div data-src="https://i.imgur.com/NQIT1JH.png" />
+    <div data-src="https://i.imgur.com/nUbzYZO.png" />
+	<div data-src="https://i.imgur.com/nO8j7cI.png" />
+	<div data-src="https://i.imgur.com/iLJbh7E.png" />
+		</AutoplaySlider>	)
+				
+return ( slider ) 
+
 }
 
-
-const linkstyle = {
-    color: '#333',
-    textDecoration: 'none'
-
-}
