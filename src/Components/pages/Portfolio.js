235fc83998c9bbac8c74b267ee01bce295e67bc7
Mohammed <44106897+Mokoan@users.noreply.github.com>
2019-11-04 11:37:23 -0600

    fuctional Awesom slidrs, issue with img sizing

--- src/Components/pages/Portfolio.js
+++ src/Components/pages/Portfolio.js
@@ -1,60 +1,33 @@
 import React from 'react'
-import { Gallery, GalleryImage } from "react-photo-gallery";
-//import { photos } from "\Photos.js";
-import PhotoGrid from "react-photo-feed";
-import "react-photo-feed/library/style.css";
-//import photos from './photos.js'
-const demoPhotos = [
-	{
-		id : 1, src : "https://i.imgur.com/IHu4dkl.png",
-		bigSrc : "https://i.imgur.com/IHu4dkl.png"
-	},
-	{
-		id : 2, src : "https://i.imgur.com/qkCCZN2.png",
-		bigSrc : "https://i.imgur.com/qkCCZN2.png"
-	},
-	{
-		id : 3, src : "https://i.imgur.com/NQIT1JH.png",
-		bigSrc : "https://i.imgur.com/NQIT1JH.png"
-	},
-	{
-		id : 4, src : "https://i.imgur.com/nUbzYZO.png",
-		bigSrc : "https://i.imgur.com/nUbzYZO.png"
-	},
-	{
-		id : 5, src : "https://i.imgur.com/nO8j7cI.png",
-		bigSrc : "https://i.imgur.com/nO8j7cI.png"
-	},
-	{
-		id : 6, src : "https://i.imgur.com/iLJbh7E.png",
-		bigSrc : "https://i.imgur.com/iLJbh7E.png"
-	}
-	
-]
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import AwesomeSliderStyles from 'react-awesome-slider/src/styled/fold-out-animation';
+
+
+
+
 export default function Portfolio() {
     return (
-       <div>
-<script type="text/javascript" src="path/to/instafeed.min.js"></script>
-            <h2 style={linkstyle}>Portfolio is under constructoin</h2>
-            
-            <PhotoGrid columns={3} photos={demoPhotos} />
-        </div>
-    )
+       
+		
+				 
+  <AwesomeSlider cssModule={AwesomeSliderStyles}>
+       <div data-src="https://i.imgur.com/IHu4dkl.png" />
+    <div data-src="https://i.imgur.com/qkCCZN2.png" />
+    <div data-src="https://i.imgur.com/NQIT1JH.png" />
+    <div data-src="https://i.imgur.com/nUbzYZO.png" />
+	<div data-src="https://i.imgur.com/nO8j7cI.png" />
+	<div data-src="https://i.imgur.com/iLJbh7E.png" />
+  </AwesomeSlider>
+);
+		
+		  
+		 
+		  
+			
+  
+
+        
+    
 }
-//import React from "react";
 
-
-
-
-// ReactDOM.render(
-// 	<div>
-// 		<PhotoGrid columns={3} photos={demoPhotos} />
-// 	</div>,
-// 	document.getElementById('root')
-// );
-
-const linkstyle = {
-    color: '#333',
-    textDecoration: 'none'
-
-}
