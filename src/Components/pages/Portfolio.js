f66e00d171c5f5a0f5f3f7664af6ae37973acf53
Jacob Steiner <jtsnwn@mail.umsl.edu>
2019-11-08 22:18:34 -0600

    incorporated more changes

--- src/Components/pages/Portfolio.js
+++ src/Components/pages/Portfolio.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import AwesomeSlider from "react-awesome-slider";
 import withAutoplay from "react-awesome-slider/dist/autoplay";
@@ -24,36 +23,3 @@ export default function Portfolio() {
 
   return slider;
 }
-=======
-import React from 'react'
-import AwesomeSlider from 'react-awesome-slider';
-import withAutoplay from 'react-awesome-slider/dist/autoplay';
-import 'react-awesome-slider/dist/styles.css';
-
-
-	
-
-export default function Portfolio () {
-   
-       
-	const AutoplaySlider = withAutoplay(AwesomeSlider);
-		const slider = (
-		  <AutoplaySlider
-			play={true}
-			cancelOnInteraction={false} // should stop playing on user interaction
-			interval={6000}
-			infinite = {true}
-		  >
-	<div data-src="https://i.imgur.com/IHu4dkl.png" />
-    <div data-src="https://i.imgur.com/qkCCZN2.png" />
-    <div data-src="https://i.imgur.com/NQIT1JH.png" />
-    <div data-src="https://i.imgur.com/nUbzYZO.png" />
-	<div data-src="https://i.imgur.com/nO8j7cI.png" />
-	<div data-src="https://i.imgur.com/iLJbh7E.png" />
-		</AutoplaySlider>	)
-				
-return ( slider ) 
-
-}
-
->>>>>>> 73b79f9a99101f361bd34424f0a6334f7ce1187d
