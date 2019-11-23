413a7597df0caf0b2960b3e9abc37e85118178a0
Mohammed <44106897+Mokoan@users.noreply.github.com>
2019-11-23 14:05:50 -0600

    Portfolio Header fix, Container on portfolio div

--- src/Components/pages/Portfolio.js
+++ src/Components/pages/Portfolio.js
@@ -5,6 +5,9 @@ import "react-awesome-slider/dist/styles.css";
 
 export default function Portfolio() {
   const AutoplaySlider = withAutoplay(AwesomeSlider);
+  
+  
+
   const slider = (
     <AutoplaySlider
       play={true}
@@ -12,12 +15,12 @@ export default function Portfolio() {
       interval={6000}
       infinite={true}
     >
-      <div data-src="https://i.imgur.com/IHu4dkl.png" />
-      <div data-src="https://i.imgur.com/qkCCZN2.png" />
-      <div data-src="https://i.imgur.com/NQIT1JH.png" />
-      <div data-src="https://i.imgur.com/nUbzYZO.png" />
-      <div data-src="https://i.imgur.com/nO8j7cI.png" />
-      <div data-src="https://i.imgur.com/iLJbh7E.png" />
+      <div data-src="https://i.ibb.co/rHVVp06/2-qk-CCZN2.png" />
+      <div data-src="https://i.ibb.co/pr1cjX4/3-NQIT1-JH.png" />
+      <div data-src="https://i.ibb.co/72J5TPq/4-n-Ubz-YZO.png"/>
+      <div data-src="https://i.ibb.co/86xTPP4/5-n-O8j7c-I.png" />
+      <div data-src="https://i.ibb.co/wgSkbtk/6-i-LJbh7-E.png" />
+      
     </AutoplaySlider>
   );
 
